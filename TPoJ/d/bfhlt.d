1f6f184525cb9be44fbdcbee709c5bbaafef9a06
bucketfulofsunshine <AEShadowHunter@gmail.com>
2026-01-29 14:42:52 +0200

    Delete TPoJ directory

--- TPoJ/d/bfhlt.d
+++
@@ -1,7 +0,0 @@
-BEGIN bfhlt
-
-IF ~True()~ THEN BEGIN bfhlt
-SAY ~This is a letter from the Helmites, inviting you to their camp.~ 
-+ ~!AreaCheck("OH5300")~ + ~Follow the directions.~ DO ~ClearAllActions() StartCutSceneMode() StartCutScene("hltport")~ EXIT
-++ ~I'll stay here for now.~ EXIT
-END
